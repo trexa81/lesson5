e0d7ed4fa77ba99a9a95a80ff32354d4321d398f
trexa81 <trex81@yandex.ru>
2021-11-13 19:05:07 +0300

    fshdhfhd

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//sfhgsdfysf
