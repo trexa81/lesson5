173bfcdaa605d3d874077ac2b4aa439c81bb9fb5
trexa81 <trex81@yandex.ru>
2021-11-13 19:17:47 +0300

    new Message

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-namespace lesson5
+namespace lesson5 
 {
     class Program
-    {
+    {   //new Message
         static void Main(string[] args)
         {
             
         }
     }
 }
-//new
